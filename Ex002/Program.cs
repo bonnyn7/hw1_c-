28e2329b81898975d4fb1814358c66b3ea196fa3
Anastasia <bonnyn7@yandex.ru>
2022-06-26 11:55:24 +0300

    Ex002 update

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -3,3 +3,19 @@
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 5;
+        int b = 7;
+
+        int max = a;
+
+        if (a > max) max = a;
+        if (b > max) max = b;
+
+        Console.Write("max= ");
+        Console.WriteLine(max);
+    }
+}
