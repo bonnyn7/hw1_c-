60c00c0f43618bf2ae9cb66b6fa011e2355eda28
Anastasia <bonnyn7@yandex.ru>
2022-06-26 17:01:54 +0300

    Added ex008

---
+++ Ex_008_1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N. (не включая N)
+//5 -> 2, 4
+//8 -> 2, 4, 6
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        int N = Convert.ToInt32(Console.ReadLine());
+        int i = 1;
+
+        for (i=1; i<N; i++)
+        {
+          if (i % 2 == 0) 
+          {
+            Console.WriteLine(i);
+          }
+        }
+    }      
+}
