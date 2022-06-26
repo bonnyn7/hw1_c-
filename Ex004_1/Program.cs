ef820a65386bc8c50e0c3aec51aa2a108523e9e6
Anastasia <bonnyn7@yandex.ru>
2022-06-26 12:33:41 +0300

    Added ex004_1

---
+++ Ex004_1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 2;
+        int b = 3;
+        int c = 7;
+
+        int max = a;
+
+        if (a > max) max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+
+        Console.Write("max= ");
+        Console.WriteLine(max);
+    }
+}
